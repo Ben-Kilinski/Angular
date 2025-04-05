85071a2357750c2303d9fc7d6dbc08a02479586e
Ben Kilinski <ben.kilinski@gmail.com>
2025-04-05 17:53:34 +0300

    header-app

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { UserComponent } from "./components/user/user.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [UserComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
