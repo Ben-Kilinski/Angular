a1ce6ef019b80c6fa4585b1fff65fbd7952b557d
Ben Kilinski <ben.kilinski@gmail.com>
2025-04-05 20:01:24 +0300

    shavua tov

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { UserComponent } from "./components/user/user.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { DUMMY_USERS } from '../dummy-users';
+import { TaskComponent } from "./components/task/task.component";
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [UserComponent, HeaderComponent],
+  imports: [UserComponent, HeaderComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId )!
+  }
+  onSelectUser(id: string) {
+    this.selectedUserId = id
+}
+}
--- 01-starting-project/src/app/components/user/user.component.ts
+++ 01-starting-project/src/app/components/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../../../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,8 +8,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
+  @Input({ required: true }) id!:string;
+  @Input({ required: true }) avatar!:string;
+  @Input({ required: true }) name!:string;
+  @Output() select = new EventEmitter(); 
+
+  get imagePath() {	
+    return 'assets/images/' + this.avatar;
   }
-}
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+
+}  
