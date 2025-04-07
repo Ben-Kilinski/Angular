49ea97d516bdcb706cc1ea3ecfe006ebebe3122e
Ben Kilinski <ben.kilinski@gmail.com>
2025-04-07 12:43:25 +0300

    baba

--- 01-starting-project/src/app/components/tasks/tasks.component.ts
+++ 01-starting-project/src/app/components/tasks/tasks.component.ts
@@ -13,25 +13,28 @@ export class TasksComponent {
   @Input({required: true}) name!: string;
   tasks = [
     {
-      id: 1,
+      id: 't1',
       userId: 'u1',
-      title: 'Task 1',
-      description: 'Description for Task 1',
-      completed: false,
-      dueDate: '2025-10-01'
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
     },
     {
-      id: 2,
-      title: 'Task 2',
-      description: 'Description for Task 2',
-      completed: true
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
     },
     {
-      id: 3,
-      title: 'Task 3',
-      description: 'Description for Task 3',
-      completed: false
-    }
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
   ]
 
     get selectedUserTasks() {
